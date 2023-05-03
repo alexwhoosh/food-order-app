956ff3edf5e0ccab31a8cc06e780f6a782b15460
alexwhoosh <gango1748@gmail.com>
2023-05-03 14:07:10 +0300

    created the bg image component and header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderContainer from "./Header.styled";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <h1>ReactMeals</h1>
+      <button>Your Cart</button>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
