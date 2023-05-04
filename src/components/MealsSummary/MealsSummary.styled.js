39f6514af0cec1dd4bd0d117cd376d0cf763ef0c
alexwhoosh <gango1748@gmail.com>
2023-05-04 17:24:36 +0300

    added summary section

---
+++ src/components/MealsSummary/MealsSummary.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+  text-align: center;
+  max-width: 45rem;
+  width: 90%;
+  margin: auto;
+  margin-top: -10rem;
+  position: relative;
+  background-color: #383838;
+  color: white;
+  border-radius: 14px;
+  padding: 1rem;
+  box-shadow: 0 1px 18px 10px rgba(0, 0, 0, 0.25);
+
+  & h2 {
+    font-size: 2rem;
+    margin-top: 1rem;
+  }
+`;
+
+export default Container;
