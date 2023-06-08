9987aac933cc6c8b7898f691b543c3d1ae34dbaf
alexwhoosh <gango1748@gmail.com>
2023-06-08 14:35:09 +0300

    styled cart item component

--- src/components/CartModal/CartItem/CartItem.js
+++ src/components/CartModal/CartItem/CartItem.js
@@ -1,18 +1,43 @@
-import ItemContainer from "./CartItem.styled";
-import AmountBadge from "./AmountBadge.styled";
+import React, { useContext } from "react";
+import AmountContext from "../../../context/amount-context";
+
+import ItemContainer, {
+  Price,
+  Amount,
+  Summary,
+  MealDetails,
+  Button,
+  ButtonsContainer,
+} from "./CartItem.styled";
 
 const CartItem = ({ name, price, amount }) => {
+  const amountCtx = useContext(AmountContext);
+
+  const addItem = (amount) => {
+    amountCtx.setAmount(++amount);
+  };
+
+  const removeItem = (amount) => {
+    amountCtx.setAmount(--amount);
+  };
+
   return (
     <ItemContainer>
-      <div>
+      <MealDetails>
         <h2>{name}</h2>
-        <span>{`$${price}`}</span>
-        <AmountBadge>{amount}</AmountBadge>
-      </div>
-      <div>
-        <button>-</button>
-        <button>+</button>
-      </div>
+        <Summary>
+          <Price>{`$${price}`}</Price>
+          <Amount>{`x ${amountCtx.amount}`}</Amount>
+        </Summary>
+      </MealDetails>
+      <ButtonsContainer>
+        <Button type="button" onClick={() => removeItem(amountCtx.amount)}>
+          -
+        </Button>
+        <Button type="button" onClick={() => addItem(amountCtx.amount)}>
+          +
+        </Button>
+      </ButtonsContainer>
     </ItemContainer>
   );
 };
